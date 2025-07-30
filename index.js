71e77d07b7227fe4d0a0465535b95bd9a2f35df4
TeejMcSteez <teejmcsteez@gmail.com>
2025-07-30 19:37:15 -0400

    feat: updated index to handle getting cut down logs by date and updated db schema for more rich information

--- index.js
+++ index.js
@@ -41,22 +41,40 @@ fastify.get("/news/:id", async function (request, reply) {
         reply.status(500).send({ error: "Database error" });
     }
 });
-fastify.get("/logs/:id", async function (request, reply) {
-    const id = Number(request.params.id);
+fastify.get("/logs/:now", async function (request, reply) {
+    const now = Number(request.params.now);
+    fastify.log.info("Date recieved:", now);
+    const baseDate = new Date(now);
+    const start = new Date(baseDate);
+    start.setUTCHours(0, 0, 0, 0);
+
+    const end = new Date(baseDate);
+    end.setUTCHours(29, 59, 59, 999);
+
     try {
-        if (!id) {
-            reply.send({ error: "ID was not valid, could not be converted to number object. What are you doing?"});
+        if (!start || !end) {
+            reply.send({ error: "Date send in UTC seconds was invalid."});
         } else {
-            const data = await db.news.findFirst({
-                where: { id }
+            const data = await db.logs.findMany({
+                where: {
+                    timestamp: {
+                        gte: start,
+                        lte: end
+                    },
+                },
+                orderBy: { timestamp: 'desc'}
             });
             reply.send({ data });
         }
     } catch (e) {
         fastify.log.error(e);
-        reply.status(500).send({ error: "Database error" });
+        reply.status(500).send({ error: "Database error"});
     }
 });
+/**
+ * add endpoints to get a certain amount of data back for filtering.
+ * makes it so the user can fetch less data rather than one or all to add to filtering logic. 
+ * */ 
 
 fastify.addHook('onClose', async (instance, done) => {
     await db.$disconnect();
